7bdba7c2c09d9876f983b2d881b6151689bb1ff6
Kh4iri <khairinajahrifky12@gmail.com>
2023-09-08 15:21:57 +0700

    Fix long floating number issue

--- src/routes/Stat.svelte
+++ src/routes/Stat.svelte
@@ -2,6 +2,15 @@
   export let name: string
   export let formula: string = ''
   export let result: string | number
+
+  $: if (typeof result === 'number') {
+    result = formatNumber(result);
+  }
+
+  function formatNumber(num: number): string {
+    const formattedNumber = num.toFixed(5);
+    return formattedNumber.replace(/\.?0*$/, '');
+  }
 </script>
 
 <span class="text-info">{name}:</span>
