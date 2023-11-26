9619ecea90f1abe99ab5405445d7f5fc76cbdd41
Kh4iri <khairinajahrifky12@gmail.com>
2023-11-26 19:03:18 +0700

    Updates

--- _app/immutable/chunks/config.3a390a09.js
+++
@@ -1,1 +0,0 @@
-const o={name:"Khairi's Math Toolbox",github:"https://github.com/Kh4iri/math-toolbox"};export{o as c};
---
+++ _app/immutable/chunks/configuration.d9601b39.js
@@ -0,0 +1,1 @@
+const t="Khairi's Math Toolbox",o="/math-toolbox",h=`https://github.com/Kh4iri${o}`;export{h as g,t as n};
