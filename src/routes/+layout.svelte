8e58b5c181174d006954fa97f71fa1cf6c3fa868
Kh4iri <khairinajahrifky12@gmail.com>
2023-11-26 19:19:07 +0700

    attempt #3 to fix github pages basepath issue

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,7 +12,7 @@
 <div class="container mx-auto max-w-xl px-4 py-6 lg:max-w-3xl lg:pt-10">
 
   <!-- Back Button -->
-  {#if $page.url.pathname !== `${base}/`}
+  {#if $page.route.id !== '/'}
     <a href="{base}/" class="mb-3 btn btn-sm">
       <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M19 12H6M12 5l-7 7 7 7"/></svg>
       Back
