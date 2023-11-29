2db596ad02f31f19430fe1e392df3513427f8730
Kh4iri <khairinajahrifky12@gmail.com>
2023-11-30 05:19:58 +0700

    Updates

---
+++ _app/immutable/chunks/paths.a8ed6b14.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_zfniyl)==null?void 0:s.base)??"/math-toolbox";var t;const e=((t=globalThis.__sveltekit_zfniyl)==null?void 0:t.assets)??a;export{e as a,a as b};
